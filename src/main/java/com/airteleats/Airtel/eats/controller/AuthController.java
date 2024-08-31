7ab5af173c981ef855241506f211a3a166e1c880
Dipen Kalsi <kalsidipen@gmail.com>
2024-08-31 19:14:19 +0530

    Initial commit

--- src/main/java/com/airteleats/Airtel/eats/controller/AuthController.java
+++ src/main/java/com/airteleats/Airtel/eats/controller/AuthController.java
@@ -2,22 +2,29 @@ package com.airteleats.Airtel.eats.controller;
 
 import com.airteleats.Airtel.eats.config.JwtProvider;
 import com.airteleats.Airtel.eats.model.Cart;
+import com.airteleats.Airtel.eats.model.USER_ROLE;
 import com.airteleats.Airtel.eats.model.User;
 import com.airteleats.Airtel.eats.repository.CartRepository;
 import com.airteleats.Airtel.eats.repository.UserRepository;
+import com.airteleats.Airtel.eats.request.LoginRequest;
 import com.airteleats.Airtel.eats.response.AuthResponse;
 import com.airteleats.Airtel.eats.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth") //endpoints for all mappings inside this controller will start from '/auth'
@@ -57,11 +64,47 @@ public class AuthController {
 
         String jwt = jwtProvider.generateToken(authentication);
         AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
         authResponse.setMessage("Registered Successfully");
         authResponse.setRole(savedUser.getRole());
 
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 
+    //signin method
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest req){
+        String username = req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Login Successful");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    //authentication for siginin
+    private Authentication authenticate(String username, String password) {
+        //trynna fetch the user from the database with his email
+        UserDetails userdetails = customUserDetailsService.loadUserByUsername(username);
+
+        //if user does not exist
+        if(userdetails==null){
+            throw new BadCredentialsException("A user does not exist with this username.");
+        }
+
+        //if the password is incorrect
+        if(!passwordEncoder.matches(password, userdetails.getPassword())){
+            throw new BadCredentialsException("Incorrect password.");
+        }
+        return new UsernamePasswordAuthenticationToken(userdetails, null, userdetails.getAuthorities());
+    }
 
 }
---
+++ src/main/java/com/airteleats/Airtel/eats/request/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.airteleats.Airtel.eats.request;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+
+}
